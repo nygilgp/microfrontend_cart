e728cfbe33fbe26970fd419047f3b1706ee4cec5
Nygil Pereira <nygilgp@gmail.com>
2021-10-05 15:09:51 +0530

    Async load scripts in cart and products to avoid script load issues.

--- cart/src/index.js
+++ cart/src/index.js
@@ -1,7 +1,1 @@
-import faker from 'faker';
-
-const cartText = `<div>You have ${faker.datatype.number(
-  10
-)} items in your cart.</div>`;
-
-document.querySelector('#dev-cart').innerHTML = cartText;
+import('./bootstrap');
